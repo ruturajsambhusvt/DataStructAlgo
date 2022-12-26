6d0fcccf6fe3d5e6b4b9721f2b2134bc8a659b65
Ruturaj Sambhus <ruturajsambhus@vt.edu>
2022-12-25 19:26:26 -0500

    Set up with cmake

---
+++ build/CMakeFiles/08_ArrayADT.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "08_ArrayADT"
+  "08_ArrayADT.pdb"
+  "CMakeFiles/08_ArrayADT.dir/08_ArrayADT.c.o"
+  "CMakeFiles/08_ArrayADT.dir/08_ArrayADT.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/08_ArrayADT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
